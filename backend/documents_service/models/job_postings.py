7439106d23476fba15582b84a802145cffc473a1
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2025-06-19 16:49:41 -0500

    feat: add sqlalchemy job postings model with full_text for vectorization

---
+++ backend/documents_service/models/job_postings.py
@@ -0,0 +1,23 @@
+import uuid
+
+from sqlalchemy import Column, String, Enum, Integer, Text
+from sqlalchemy.dialects.postgresql import UUID
+
+from ..database.database import Base
+
+class JobCategory(Enum):
+    DATA = 'data'
+    CLOUD = 'cloud'
+    OTHERS = 'others'
+
+class JobPostings(Base):
+    __tablename__ = 'job_postings'
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    recruiter_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4),
+    title = Column(String, nullable=False)
+    years_of_experience = Column(Integer, nullable=False)
+    category = Column(Enum(JobCategory), nullable=False)
+    company_name = Column(String, nullable=False)
+    requirements = Column(String, nullable=False)
+    full_description = Column(Text, nullable=False)
