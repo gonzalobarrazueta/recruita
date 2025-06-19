482a6451e63efdfa826d1a70fcb32582f0c616f6
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2025-06-19 13:21:52 -0500

    feat(frontend): add Chat component and route

--- frontend/recruita/src/app/app.routes.ts
+++ frontend/recruita/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { SignUp } from './features/auth/components/sign-up/sign-up';
 import { Login } from './features/auth/components/login/login';
 import { JobPostings } from './features/applicant/components/job-postings/job-postings';
 import { JobListings } from './features/recruiter/job-listings/job-listings';
+import { Chat } from './shared/components/chat/chat';
 
 export const routes: Routes = [
   { path: '', component: SignUp },
   { path: 'sign-up', component: SignUp },
   { path: 'login', component: Login },
   { path: 'jobs', component: JobPostings },
-  { path: 'manage-jobs', component: JobListings }
+  { path: 'manage-jobs', component: JobListings },
+  { path: 'chat', component: Chat }
 ];
