c73e9d8f8dfdc54858b645504dc07ad76b8315ee
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2025-06-24 10:04:29 -0500

    feat: implement reactive form for message input and agent interaction
    - Add FormGroup to manage message input using reactive forms
    - Create sendMessage method to handle form submission and communicate with the agent via HTTP

--- frontend/recruita/src/app/shared/components/chat/chat.ts
+++ frontend/recruita/src/app/shared/components/chat/chat.ts
@@ -1,13 +1,37 @@
 import { Component } from '@angular/core';
 import { Message } from '../../../models/message';
+import {Agent} from '../../services/agent';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-chat',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './chat.html',
   styleUrl: './chat.scss'
 })
 export class Chat {
 
+  chatForm!: FormGroup;
   messages: Message[] = [];
+
+  constructor(private formBuilder: FormBuilder, private agentService: Agent) {
+    this.chatForm = this.formBuilder.group({
+      userInput: ['', Validators.required]
+    });
+  }
+
+  sendMessage() {
+    let userInput: string = this.chatForm.get('userInput')?.value;
+
+    if (userInput) {
+      this.agentService.sendMessage(userInput)
+        .subscribe(response => {
+          console.log(response);
+        })
+    }
+
+    this.chatForm.reset();
+  }
 }
