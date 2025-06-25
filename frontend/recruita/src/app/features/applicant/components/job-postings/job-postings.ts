ca49afa94978a6a71000adce6cf8718d8f2b39ff
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2025-06-25 10:23:53 -0500

    feat: fetch job postings from jobsService on component initialization

--- frontend/recruita/src/app/features/applicant/components/job-postings/job-postings.ts
+++ frontend/recruita/src/app/features/applicant/components/job-postings/job-postings.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { JobPosting } from '../../../../models/job-posting';
+import { Jobs } from '../../../../shared/services/jobs';
 
 @Component({
   selector: 'app-job-postings',
@@ -11,6 +12,12 @@ export class JobPostings {
 
   jobPostings: JobPosting[] = [];
 
+  constructor(private jobsService: Jobs) {
+    this.jobsService.getJobs().subscribe(jobs => {
+      this.jobPostings = jobs;
+    });
+  }
+
   @ViewChild('scrollableList', { static: false }) scrollContainer!: ElementRef;
 
   scrollRight(){
@@ -19,6 +26,5 @@ export class JobPostings {
 
   scrollLeft(){
     this.scrollContainer.nativeElement.scrollBy({ left: -300, behavior: 'smooth' });
-
   }
 }
