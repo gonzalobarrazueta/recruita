dba3aab81e777016e0b1f9a70ff32372deb436cc
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2025-07-29 14:39:13 -0500

    feat: add Candidate Profile component

---
+++ frontend/recruita/src/app/features/recruiter/candidate-profile/candidate-profile.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-candidate-profile',
+  imports: [],
+  templateUrl: './candidate-profile.html',
+  styleUrl: './candidate-profile.scss'
+})
+export class CandidateProfile {
+
+}
