0751894480f5d8894482c88073a1bd31d59002d6
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2025-08-06 18:05:11 -0500

    feat(agent): create Conversations model

---
+++ agent/models/conversations.py
@@ -0,0 +1,17 @@
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import Column, DateTime
+from datetime import datetime, UTC
+
+from sqlalchemy.orm import relationship
+
+from ..database import Base
+import uuid
+
+class Conversations(Base):
+    __tablename__ = "conversations"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    user_id = Column(UUID(as_uuid=True), nullable=False)
+    job_posting_id = Column(UUID(as_uuid=True), nullable=False)
+    created_at = Column(DateTime, default=datetime.now(UTC), nullable=False)
+    messages = relationship("Messages", backref="conversation", cascade="all, delete")
