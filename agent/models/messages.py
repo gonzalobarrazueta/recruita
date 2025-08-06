4d329fbe4052ea299875ac5ed5e8a3c9dc4c5111
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2025-08-06 18:02:11 -0500

    feat(agent): create Messages model

---
+++ agent/models/messages.py
@@ -0,0 +1,21 @@
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import Column, DateTime, String, ForeignKey, Enum
+from datetime import datetime, UTC
+from ..database import Base
+import uuid
+import enum
+
+class Sender(enum.Enum):
+    USER = 'user'
+    AI = 'ai'
+
+class Messages(Base):
+    __tablename__ = "messages"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    user_id = Column(UUID(as_uuid=True), nullable=False)
+    job_posting_id = Column(UUID(as_uuid=True), nullable=False)
+    conversation_id = Column(UUID(as_uuid=True), ForeignKey('conversations.id'), nullable=False)
+    created_at = Column(DateTime, default=datetime.now(UTC), nullable=False)
+    content = Column(String, nullable=False)
+    sender = Column(Enum(Sender), nullable=False)
