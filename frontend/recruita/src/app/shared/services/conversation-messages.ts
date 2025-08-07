33f8d6b3c16073af74d2e9cf7b4f847e556acc1f
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2025-08-07 16:47:27 -0500

    feat: add createMessage method

--- frontend/recruita/src/app/shared/services/conversation-messages.ts
+++ frontend/recruita/src/app/shared/services/conversation-messages.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../../environments/environment';
+import {Message} from '../../models/message';
 
 @Injectable({
   providedIn: 'root'
@@ -11,5 +12,9 @@ export class ConversationMessages {
 
   getConversation(userId: string, jobPostingId: string) {
     return this.http.get(`${environment.AGENT_API_URL}/conversations/${userId}/${jobPostingId}`);
+  }
+
+  createMessage(message: Message) {
+    return this.http.post(`${environment.AGENT_API_URL}/messages`, message);
   }
 }
