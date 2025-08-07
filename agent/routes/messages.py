e132f903905fab629c78d599a12f436ad9978204
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2025-08-07 16:10:19 -0500

    feat: add Messages route

---
+++ agent/routes/messages.py
@@ -0,0 +1,38 @@
+from fastapi import APIRouter, Depends, Request
+from sqlalchemy.orm import Session
+from starlette import status
+from typing import Annotated
+from uuid import UUID
+from ..database.database import get_db
+from ..models.conversations import Conversations
+from ..models.messages import Messages, Sender
+from ..services.conversations import get_or_create_conversation
+
+router = APIRouter(
+    prefix='/messages',
+    tags=['messages']
+)
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@router.post(
+    path='/messages',
+    status_code=status.HTTP_201_CREATED
+)
+async def create_message(request: Request, db: db_dependency):
+    data = await request.json()
+    user_id: UUID = data['user_id']
+    conversation_id: UUID = data['conversation_id']
+    content: str = data['content']
+    sender: Sender = data['sender']
+
+    message = Messages(
+        user_id=user_id,
+        conversation_id=conversation_id,
+        content=content,
+        sender=Sender(sender)
+    )
+
+    db.add(message)
+    db.commit()
+    db.refresh(message)
