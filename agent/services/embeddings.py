8af4b8f00e441274a30117936bc58099b8d988b6
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2025-08-09 18:46:44 -0500

    feat(agent): add embeddings service with methods that vectorize text and store embeddings in a Pinecone vector database

---
+++ agent/services/embeddings.py
@@ -0,0 +1,52 @@
+from pinecone import Pinecone, ServerlessSpec
+from openai import OpenAI
+from dotenv import load_dotenv
+from uuid import uuid4
+import os
+
+load_dotenv()
+
+oai_client = OpenAI(api_key=os.environ['OPENAI_API_KEY'])
+pc = Pinecone(api_key=os.environ['PINECONE_API_KEY'])
+messages_index_name='messages'
+
+def create_pinecone_index(index_name: str):
+    pc.create_index(
+        name=index_name,
+        vector_type='dense',
+        dimension=1536,
+        metric='cosine',
+        spec=ServerlessSpec(
+            cloud='aws',
+            region='us-east-1'
+        ),
+        deletion_protection='disabled',
+        tags={
+            'environment': 'production'
+        }
+    )
+
+def get_index(index_name: str):
+    if not pc.has_index(index_name):
+        create_pinecone_index(index_name)
+    return pc.Index(index_name)
+
+def embed_text(text: str):
+    return oai_client.embeddings.create(
+        input=text,
+        model='text-embedding-3-small'
+    ).data[0].embedding
+
+def store_embeddings(user_prompt: str, conversation_id: str):
+
+    emb = embed_text(user_prompt)
+
+    index = get_index(messages_index_name)
+
+    index.upsert(
+        vectors=[{
+            'id': str(uuid4()),
+            'values': emb,
+            'metadata': {'conversation_id': conversation_id}
+        }]
+    )
