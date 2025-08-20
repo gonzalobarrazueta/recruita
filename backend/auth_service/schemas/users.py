b478532f1e78d88998d959ed9c4c2f42c91194eb
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2025-08-20 17:50:18 -0500

    feat(agent): add UserResponse schema

--- backend/auth_service/schemas/users.py
+++ backend/auth_service/schemas/users.py
@@ -8,3 +8,14 @@ class CreateUserRequest(BaseModel):
     role: str
     password: str
     organization: str
+
+class UserResponse(BaseModel):
+    name: str
+    last_name: str
+    phone_number: str
+    email: str
+    role: str
+    organization: str
+
+    class Config:
+        from_attributes = True
