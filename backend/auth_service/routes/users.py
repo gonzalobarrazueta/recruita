7e5a103aabf0e06a02db8b6fff3f6500e1f112a3
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2025-09-15 21:20:43 -0500

    feat(agent): add endpoint to check whether a user has accepted the terms and conditions or not

--- backend/auth_service/routes/users.py
+++ backend/auth_service/routes/users.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from starlette import status
-from typing import Annotated
+from typing import Annotated, Dict
 from sqlalchemy.orm import Session
 
 from ..database import SessionLocal
@@ -32,3 +32,12 @@ def get_user_by_id(user_id: str, db: db_dependency):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User not found')
 
     return result
+
+@router.get(
+    path='/terms-and-conditions/{user_id}',
+    response_model=Dict,
+    status_code=status.HTTP_200_OK
+)
+def check_user_terms_conditions(user_id: str, db: db_dependency):
+    user = db.query(Users).filter(Users.id == user_id).first()
+    return {'has_accepted': user.terms_conditions_accepted}
