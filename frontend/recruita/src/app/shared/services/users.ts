df0d60a5fe34c2ec4f1a36b0368f2dceb034fabe
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2025-09-17 19:38:24 -0500

    feat: add methods to check and update terms_and_conditions acceptance

--- frontend/recruita/src/app/shared/services/users.ts
+++ frontend/recruita/src/app/shared/services/users.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../../environments/environment';
 import {Observable} from 'rxjs';
+import {HttpResponse} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +15,13 @@ export class Users {
 
   getUserById(user_id: string): Observable<any> {
     return this.http.get(`${this.usersApiURL}/${user_id}`);
+  }
+
+  checkTermsAndConditions(user_id: string) {
+    return this.http.get(`${this.usersApiURL}/terms-and-conditions/${user_id}`);
+  }
+
+  acceptTermsAndConditions(user_id: string, accept: boolean): Observable<HttpResponse<any>> {
+    return this.http.patch<any>(`${this.usersApiURL}/terms-and-conditions/${user_id}`, { accept }, { observe: 'response' });
   }
 }
