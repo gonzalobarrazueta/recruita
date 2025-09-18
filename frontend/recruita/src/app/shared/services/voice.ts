cf81e1232d4ac4401bf88a4c6540d3c8373bf39e
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2025-09-18 12:37:06 -0500

    feat: add Voice service

---
+++ frontend/recruita/src/app/shared/services/voice.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../../environments/environment.development';
+
+interface TranscriptionResponse {
+  transcription: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Voice {
+
+  private voiceApiURL: string = `${environment.AGENT_API_URL}/audio`;
+
+  constructor(private http: HttpClient) { }
+
+  voiceToText(audioFile: File) {
+    const formData = new FormData();
+    formData.append("audio_file", audioFile);
+    console.log("filetype:", audioFile.type);
+
+    return this.http.post<TranscriptionResponse>(this.voiceApiURL, formData);
+  }
+}
