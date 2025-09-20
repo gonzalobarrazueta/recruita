f75fe940a40fac47e57d71825ca81447a2754448
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2025-09-20 18:44:20 -0500

    feat(api-gateway): add CORS middleware to allow cross-origin requests

--- gateway/main.py
+++ gateway/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI, Request, Response
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 import logging
 import httpx
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Base URLs for your internal container apps
 SERVICE1_AUTH_URL = "http://auth:8000"
