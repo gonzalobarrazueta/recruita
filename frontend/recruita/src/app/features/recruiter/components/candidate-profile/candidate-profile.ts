5ad2de32db26f68029edf5546241e9d8fd6d986e
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2025-09-24 20:02:33 -0500

    feat(frontend): add CandidateProfile component

--- frontend/recruita/src/app/features/recruiter/components/candidate-profile/candidate-profile.ts
+++ frontend/recruita/src/app/features/recruiter/components/candidate-profile/candidate-profile.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, Signal, signal} from '@angular/core';
+import {Users} from '../../../../shared/services/users';
+import {ActivatedRoute} from '@angular/router';
+import {Applicant} from '../../../../models/applicant';
 
 @Component({
   selector: 'app-candidate-profile',
@@ -8,4 +11,28 @@ import { Component } from '@angular/core';
 })
 export class CandidateProfile {
 
+  userId = signal("");
+  candidate: Applicant = {} as Applicant;
+
+  constructor(
+    private usersService: Users,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.activatedRoute.params.subscribe(
+      (params) => {
+        this.userId.set(params["id"]);
+
+        this.usersService.getUserById(this.userId())
+          .subscribe(data => {
+            this.candidate.userId = data["id"];
+            this.candidate.pfp = data["pfp_image"];
+            this.candidate.name = `${data["name"]} ${data["last_name"]}`;
+            this.candidate.email = data["email"];
+            this.candidate.phoneNumber = data["phone_number"];
+          });
+      }
+    );
+  }
 }
