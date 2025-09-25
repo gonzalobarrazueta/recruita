0c99e0aeced793e254a7f1164d20df95ecdc426b
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2025-09-25 13:20:21 -0500

    refactor(frontend): delete placeholders

--- frontend/recruita/src/environments/environment.ts
+++ frontend/recruita/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  AUTH_API_URL: '%%AUTH_API_URL%%',
-  DOCUMENTS_API_URL: '%%DOCUMENTS_API_URL%%',
-  AGENT_API_URL: '%%AGENT_API_URL%%'
+  AUTH_API_URL: '',
+  DOCUMENTS_API_URL: '',
+  AGENT_API_URL: ''
 };
